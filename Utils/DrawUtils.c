2c8d40d7a7b63fcd739be10c3baf10a2f10b03ae
Borealin <308704199deniel@gmail.com>
2019-05-09 16:52:05 +0800

    [Feature]Refactor DrawUtils

--- Utils/DrawUtils.c
+++ Utils/DrawUtils.c
@@ -5,21 +5,38 @@
 #include <stdlib.h>
 #include <math.h>
 #include <DrawUtils.h>
+#include <ListUtils.h>
+#include <JudgeUtils.h>
 #include "graphics.h"
 #include "imgui.h"
 #include "extgraph.h"
 #include <windef.h>
 #include <wingdi.h>
 
-double BlockLength = 0.3;
+#define DEFAULT_COLOR "Black"
+#define FRAME_COLOR "Gray"
 
-Point FrameLeftCorner;
+const double BlockLength = 0.3;
 
-int Direction[4][2] = {
+DoublePoint FrameLeftCorner;
+
+int Direction[16][2] = {
+        {0,  0},
         {-1, 0},
+        {-2, 0},
+        {-1, 1},
+        {0,  0},
         {0,  1},
+        {0,  2},
+        {1,  1},
+        {0,  0},
         {1,  0},
-        {0,  -1}
+        {2,  0},
+        {1,  -1},
+        {0,  0},
+        {0,  -1},
+        {0,  -2},
+        {-1, -1}
 };
 
 int Score;
@@ -37,109 +54,59 @@ char *TetrominoColor[8] = {
         "Red"
 };
 
+int TetrominoShape[8][4] = {
+        {0, 0, 0,  0},
+        {0, 1, 9,  10},
+        {0, 1, 3,  9},
+        {0, 1, 9,  7},
+        {0, 9, 11, 13},
+        {0, 1, 5,  7},
+        {0, 1, 5,  9},
+        {0, 3, 5,  9}
+};
+
+int TetrominoDirectionMod[8] = {
+        1, 2, 4, 4, 1, 2, 4, 2
+};
+
 void DrawBlocks(int x, int y, int r, int c, char *InnerColor, char *OuterColor);
 
 void DrawTetromino(Tetromino x) {
-    SetPenColor(TetrominoColor[x.type]);
-    switch (x.type) {
-        case 1:
-            x.direction = x.direction % 2;
-            DrawBlocks(x.x, x.y, 1, 1, TetrominoColor[x.type], "Black");
-            DrawBlocks(x.x + Direction[x.direction][0], x.y + Direction[x.direction][1], 1, 1, TetrominoColor[x.type],
-                       "Black");
-            DrawBlocks(x.x + Direction[(x.direction + 2) % 4][0], x.y + Direction[(x.direction + 2) % 4][1], 1, 1,
-                       TetrominoColor[x.type], "Black");
-            DrawBlocks(x.x + 2 * Direction[(x.direction + 2) % 4][0], x.y + 2 * Direction[(x.direction + 2) % 4][1], 1,
-                       1, TetrominoColor[x.type], "Black");
-            break;
-        case 2:
-            DrawBlocks(x.x, x.y, 1, 1, TetrominoColor[x.type], "Black");
-            DrawBlocks(x.x + Direction[x.direction][0], x.y + Direction[x.direction][1], 1, 1, TetrominoColor[x.type],
-                       "Black");
-            DrawBlocks(x.x + Direction[x.direction][0] + Direction[(x.direction + 1) % 4][0],
-                       x.y + Direction[x.direction][1] + Direction[(x.direction + 1) % 4][1], 1, 1,
-                       TetrominoColor[x.type], "Black");
-            DrawBlocks(x.x + Direction[(x.direction + 2) % 4][0], x.y + Direction[(x.direction + 2) % 4][1], 1, 1,
-                       TetrominoColor[x.type], "Black");
-            break;
-        case 3:
-            DrawBlocks(x.x, x.y, 1, 1, TetrominoColor[x.type], "Black");
-            DrawBlocks(x.x + Direction[x.direction][0], x.y + Direction[x.direction][1], 1, 1, TetrominoColor[x.type],
-                       "Black");
-            DrawBlocks(x.x + Direction[(x.direction + 2) % 4][0] + Direction[(x.direction + 1) % 4][0],
-                       x.y + Direction[(x.direction + 2) % 4][1] + Direction[(x.direction + 1) % 4][1], 1, 1,
-                       TetrominoColor[x.type], "Black");
-            DrawBlocks(x.x + Direction[(x.direction + 2) % 4][0], x.y + Direction[(x.direction + 2) % 4][1], 1, 1,
-                       TetrominoColor[x.type], "Black");
-            break;
-        case 4:
-            DrawBlocks(x.x, x.y, 1, 1, TetrominoColor[x.type], "Black");
-            DrawBlocks(x.x + 1, x.y, 1, 1, TetrominoColor[x.type], "Black");
-            DrawBlocks(x.x, x.y + 1, 1, 1, TetrominoColor[x.type], "Black");
-            DrawBlocks(x.x + 1, x.y + 1, 1, 1, TetrominoColor[x.type], "Black");
-            break;
-        case 5:
-            x.direction = x.direction % 2;
-            DrawBlocks(x.x, x.y, 1, 1, TetrominoColor[x.type], "Black");
-            DrawBlocks(x.x + Direction[(x.direction + 2) % 4][0], x.y + Direction[(x.direction + 2) % 4][1], 1, 1,
-                       TetrominoColor[x.type], "Black");
-            DrawBlocks(x.x + Direction[(x.direction + 3) % 4][0], x.y + Direction[(x.direction + 3) % 4][1], 1, 1,
-                       TetrominoColor[x.type], "Black");
-            DrawBlocks(x.x + Direction[x.direction][0] + Direction[(x.direction + 3) % 4][0],
-                       x.y + Direction[x.direction][1] + Direction[(x.direction + 3) % 4][1], 1, 1,
-                       TetrominoColor[x.type], "Black");
-            break;
-        case 6:
-            DrawBlocks(x.x, x.y, 1, 1, TetrominoColor[x.type], "Black");
-            DrawBlocks(x.x + Direction[x.direction][0], x.y + Direction[x.direction][1], 1, 1,
-                       TetrominoColor[x.type], "Black");
-            DrawBlocks(x.x + Direction[(x.direction + 1) % 4][0], x.y + Direction[(x.direction + 1) % 4][1], 1, 1,
-                       TetrominoColor[x.type], "Black");
-            DrawBlocks(x.x + Direction[(x.direction + 2) % 4][0], x.y + Direction[(x.direction + 2) % 4][1], 1, 1,
-                       TetrominoColor[x.type], "Black");
-            break;
-        case 7:
-            x.direction = x.direction % 2;
-            DrawBlocks(x.x, x.y, 1, 1, TetrominoColor[x.type], "Black");
-            DrawBlocks(x.x + Direction[x.direction][0], x.y + Direction[x.direction][1], 1, 1,
-                       TetrominoColor[x.type], "Black");
-            DrawBlocks(x.x + Direction[(x.direction + 3) % 4][0], x.y + Direction[(x.direction + 3) % 4][1], 1, 1,
-                       TetrominoColor[x.type], "Black");
-            DrawBlocks(x.x + Direction[(x.direction + 2) % 4][0] + Direction[(x.direction + 3) % 4][0],
-                       x.y + Direction[(x.direction + 2) % 4][1] + Direction[(x.direction + 3) % 4][1], 1, 1,
-                       TetrominoColor[x.type], "Black");
-            break;
-        default:
-            break;
+    int dir = x.direction % TetrominoDirectionMod[x.type];
+    for (int i = 0; i < 4; i++) {
+        DrawBlocks(x.x + Direction[(TetrominoShape[x.type][i] + 4 * dir) % 16][0],
+                   x.y + Direction[(TetrominoShape[x.type][i] + 4 * dir) % 16][1],
+                   1, 1, TetrominoColor[x.type],
+                   DEFAULT_COLOR);
     }
 }
 
 void DrawFrame(double x, double y) {
     FrameLeftCorner.x = x;
     FrameLeftCorner.y = y;
-    SetPenColor("Black");
-    DrawBlocks(0, 0, 26, 2, "White", "Black");
+    SetPenColor(DEFAULT_COLOR);
+    DrawBlocks(0, 0, 26, 2, FRAME_COLOR, DEFAULT_COLOR);
     int column[] = {
             0, 7, 18, 25
     };
     for (int i = 0; i < sizeof(column) / sizeof(column[0]); i++) {
-        DrawBlocks(column[i], 2, 1, 20, "White", "Black");
+        DrawBlocks(column[i], 2, 1, 20, FRAME_COLOR, DEFAULT_COLOR);
     }
-    DrawBlocks(19, 2, 6, 11, "White", "Black");
-    DrawBlocks(1, 2, 6, 8, "White", "Black");
-    DrawBlocks(19, 2, 6, 11, "White", "Black");
-    DrawBlocks(0, 22, 26, 2, "White", "Black");
+    DrawBlocks(19, 2, 6, 11, FRAME_COLOR, DEFAULT_COLOR);
+    DrawBlocks(1, 2, 6, 8, FRAME_COLOR, DEFAULT_COLOR);
+    DrawBlocks(19, 2, 6, 11, FRAME_COLOR, DEFAULT_COLOR);
+    DrawBlocks(0, 22, 26, 2, FRAME_COLOR, DEFAULT_COLOR);
     SetPenColor("Red");
     drawBox(x + 1 * BlockLength, y + 19 * BlockLength, BlockLength * 6, BlockLength * 3, 0, "Score", 'M', "Red");
     drawBox(x + 1 * BlockLength, y + 13 * BlockLength, BlockLength * 6, BlockLength * 3, 0, "Level", 'M', "Red");
     drawBox(x + 19 * BlockLength, y + 19 * BlockLength, BlockLength * 6, BlockLength * 3, 0, "Next One", 'M', "Red");
-    setTextBoxColors("Blue", "Red", "Blue", "Red", 0);
-    char *ScoreText = (char*)malloc(sizeof(char));
-    char *LevelText = (char*)malloc(sizeof(char));
+    char *ScoreText = (char *) malloc(sizeof(char));
+    char *LevelText = (char *) malloc(sizeof(char));
     itoa(Score, ScoreText, 10);
     itoa(Level, LevelText, 10);
-    textbox(GenUIID(0), x + 1 * BlockLength, y + 16 * BlockLength, BlockLength * 6, BlockLength * 3, ScoreText, 20);
-    textbox(GenUIID(1), x + 1 * BlockLength, y + 10 * BlockLength, BlockLength * 6, BlockLength * 3, LevelText, 20);
+    SetPenColor("Blue");
+    drawBox(x + 1 * BlockLength, y + 16 * BlockLength, BlockLength * 6, BlockLength * 3, 0, ScoreText, 'L', "Blue");
+    drawBox(x + 1 * BlockLength, y + 10 * BlockLength, BlockLength * 6, BlockLength * 3, 0, LevelText, 'L', "Blue");
 }
 
 
@@ -156,6 +123,31 @@ void DrawBlocks(int x, int y, int r, int c, char *InnerColor, char *OuterColor) 
         for (int j = y; j < y + c; ++j) {
             drawRectangle(FrameLeftCorner.x + i * BlockLength, FrameLeftCorner.y + j * BlockLength, BlockLength,
                           BlockLength, 0);
+            drawRectangle(FrameLeftCorner.x + (i + 0.25) * BlockLength, FrameLeftCorner.y + (j + 0.25) * BlockLength,
+                          BlockLength / 2,
+                          BlockLength / 2, 0);
+            MovePen(FrameLeftCorner.x + i * BlockLength, FrameLeftCorner.y + j * BlockLength);
+            DrawLine(BlockLength / 4, BlockLength / 4);
+            MovePen(FrameLeftCorner.x + (i + 1) * BlockLength, FrameLeftCorner.y + j * BlockLength);
+            DrawLine((-1) * BlockLength / 4, BlockLength / 4);
+            MovePen(FrameLeftCorner.x + i * BlockLength, FrameLeftCorner.y + (j + 1) * BlockLength);
+            DrawLine(BlockLength / 4, (-1) * BlockLength / 4);
+            MovePen(FrameLeftCorner.x + (i + 1) * BlockLength, FrameLeftCorner.y + (j + 1) * BlockLength);
+            DrawLine((-1) * BlockLength / 4, (-1) * BlockLength / 4);
+        }
+    }
+
+}
+
+void DrawLayers(int head[12][22]) {
+    if(!head[0][0]){
+        LayerInit();
+    }
+    for (int j = 1; j < 21; ++j) {
+        for (int i = 1; i < 11; ++i) {
+            if (head[i][j]) {
+                DrawBlocks(X_CORNER + i, Y_CORNER + j, 1, 1, TetrominoColor[head[i][j]], DEFAULT_COLOR);
+            }
         }
     }
 }
--- Utils/DrawUtils.h
+++ Utils/DrawUtils.h
@@ -5,20 +5,30 @@
 #ifndef BIGWORK_DRAWUTILS_H
 #define BIGWORK_DRAWUTILS_H
 
+int Direction[16][2];
+int TetrominoShape[8][4];
+int TetrominoDirectionMod[8];
+
 typedef struct {
-    int x,y;
+    int x, y;
     int direction;
     int type;
-}Tetromino;
+} Tetromino;
+
+typedef struct {
+    int x, y;
+} Point;
 
 typedef struct {
     double x, y;
-} Point;
+} DoublePoint;
 
-double BlockLength;
+const double BlockLength;
 
 void DrawTetromino(Tetromino x);
 
 void DrawFrame(double x, double y);
+
+void DrawLayers(int head[12][22]);
 
 #endif //BIGWORK_DRAWUTILS_H
